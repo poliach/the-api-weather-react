75274b079a71cca441efedce86f05dc50805f3c2
Polina Loz <pollozov@gmail.com>
2023-01-06 17:56:13 +0100

    hook in progress

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -1,12 +1,68 @@
 import { useState } from "react";
+import axios from 'axios';
+
+import getCurrentDayForecast from '../helpers/getCurrentDayForecast';
+import getCurrentDayDetailedForecast from '../helpers/getCurrentDayDetailedForecast';
+import getUpcomingDaysForecast from '../helpers/getUpcomingDaysForecast';
+
+const BASE_URL = 'http://api.openweathermap.org'
 
 const useForcast = () => {
     const [isError, setError] = useState(false)
     const [isLoading, setLoading] = useState(false)
     const [forecast, setForecast] = useState(null)
 
-    const submitRequest = location => {
-        console.log({location})
+    // api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={API key}
+    //REACT_APP_API_KEY
+
+    //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+    const getSearchOptions = async location => {
+        const { data } = await axios(`${BASE_URL}/search`, { params: { query: location } });
+
+        console.log({data})
+
+        if (!data || data.length === 0) {
+            setError('There is no such location');
+            setLoading(false);
+            return;
+        }
+
+        return data[0];
+    };
+
+    const getForecastData = async woeid => {
+        const { data } = await axios(`${BASE_URL}/${woeid}`);
+
+        if (!data || data.length === 0) {
+            setError('Something went wrong');
+            setLoading(false);
+            return;
+        }
+
+        return data;
+    };
+
+    const gatherForecastData = data => {
+        const currentDay = getCurrentDayForecast(data.consolidated_weather[0], data.title);
+        const currentDayDetails = getCurrentDayDetailedForecast(data.consolidated_weather[0]);
+        const upcomingDays = getUpcomingDaysForecast(data.consolidated_weather);
+
+        setForecast({ currentDay, currentDayDetails, upcomingDays });
+        setLoading(false);
+    };
+
+    const submitRequest = async location => {
+        setLoading(true);
+        setError(false);
+
+        const response = await getWoeid(location);
+        if (!response?.woeid) return;
+
+        const data = await getForecastData(response.woeid);
+        if (!data) return;
+
+        gatherForecastData(data);
     };
 
     return {
