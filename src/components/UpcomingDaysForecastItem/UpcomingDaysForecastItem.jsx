12d5311f9688637e713da99dc184b0c28d36bc42
Polina Loz <pollozov@gmail.com>
2023-01-07 16:39:27 +0100

    upcomingDays done

--- src/components/UpcomingDaysForecastItem/UpcomingDaysForecastItem.jsx
+++ src/components/UpcomingDaysForecastItem/UpcomingDaysForecastItem.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './UpcomingDaysForecastItem.module.css';
 
-const UpcomingDaysForecastItem = () => (
-    <li className={`${styles.weekday} d-flex flex-column justify-content-center align-items-center p-2`}></li>
+const imgUrlBase = 'http://openweathermap.org/img/';
+
+//`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+
+const UpcomingDaysForecastItem = ({ weekday, temperature, imgUrl }) => (
+    <li className={`${styles.weekday} d-flex flex-column justify-content-center align-items-center p-2`}>
+        <img className="mb-2" width="30" src={`${imgUrlBase}wn/${data.weather[0].icon}@2x.png`} alt="" />
+        <span className="mb-2">{weekday}</span>
+        <span className="font-weight-bold">{temperature}&deg;</span>
+    </li>
 );
+
+UpcomingDaysForecastItem.propTypes = {
+    weekday: PropTypes.string.isRequired,
+    temperature: PropTypes.string.isRequired,
+    imgUrl: PropTypes.string.isRequired,
+};
 
 export default UpcomingDaysForecastItem;
